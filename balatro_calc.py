153ce4957cae7454fc7c5fafca3bbf1a8fff95a7
Rou-Dou <mahoney_matthew@live.com>
2024-06-25 13:12:32 -0400

    new commit

--- balatro_calc.py
+++ balatro_calc.py
@@ -1,39 +1,72 @@
 from enum import Enum
 from uuid import uuid4
 
-class Suit(Enum):
+hand_size = 7
+
+card_values = {
+    "Ace" : 11,
+    "Two" : 2,
+    "Three" : 3,
+    "Four" : 4,
+    "Five" : 5,
+    "Six" : 6,
+    "Seven" : 7,
+    "Eight" : 8,
+    "Nine" : 9,
+    "Ten" : 10,
+    "Jack" : 10,
+    "Queen" : 10,
+    "King" : 10
+}
+
+class Suits(Enum):
     Spade = 1
     Diamond = 2
     Club = 3
     Heart = 4
 
-class Face(Enum):
-    One = 1
-    Two = 2
-    Three = 3
-    Four = 4
-    Five = 5
-    Six = 6
-    Seven = 7
-    Eight = 8
-    Nine = 9
-    Ten = 10
-    Jack = 11
-    Queen = 12
-    King = 13
-    Ace = 14
+class Faces(Enum):
+    Two = 1
+    Three = 2
+    Four  = 3
+    Five = 4
+    Six = 5
+    Seven = 6
+    Eight = 7
+    Nine  = 8
+    Ten = 9
+    Jack = 10
+    Queen = 11
+    King  = 12
+    Ace = 13
 
+class HandTypes(Enum):
+    High_Card = 1
+    Pair = 2
+    Two_Pair = 3
+    Three_Of_A_Kind = 4
+    Straight = 5
+    Flush = 6
+    Full_House = 7
+    Four_of_a_Kind = 8
+    Straight_Flush = 9
+    Five_of_a_Kind = 10
+    Flush_House = 12
+    Five_Flush = 13
 
 
 class Card:
-    def __init__(self, value, face, suit, enhancement, aura, seal):
+    def __init__(self, value, face, suit, enhancement, edition, seal):
         self.id = uuid4().hex
         self.value = value
         self.face = face
         self.suit = suit
         self.enhancement = enhancement
-        self.aura = aura
+        self.edition = edition
         self.seal = seal
+
+    def __str__(self):
+        return f'{self.face} of {self.suit}'
     
     def add_effect(self, effect):
         self.enhancement = effect
@@ -52,10 +85,32 @@ class Card:
         if self.enhancement == 'bonus':
             return True
         return False
+    
+    def is_mult(self):
+        if self.enhancement == 'mult':
+            return True
+        return False
+    
+    def is_lucky(self):
+        if self.enhancement == 'lucky':
+            return True
+        return False
+    
+    def is_wild(self):
+        if self.enhancement == 'wild':
+            return True
+        return False
 
+    def is_stone(self):
+        if self.enhancement == 'stone':
+            return True
+        return False
+    
+    def is_glass(self):
+        if self.enhancement == 'glass':
+            return True
+        return False
             
-
-
 
 class Hand:
     def __init__(self, cards: list[Card]):
@@ -69,8 +124,8 @@ class PlayedHand:
         self.score = self.chips * self.mult
 
 class Deck:
-    def __init__(self, cards: list[Card]):
-        self.cards = cards
+    def __init__(self):
+        self.cards = []
         self.length = len(self.cards)
     
     def add_card(self, card):
@@ -83,7 +138,6 @@ class Voucher:
     pass
 
 class Joker:
-
     name: str = ''
     effect: str = ''
 
@@ -104,5 +158,23 @@ class OGJoker(Joker):
 
         
 planets: list[str] = []
-jokers: list[Joker] = []
+jokers: list[Joker] = [OGJoker()]
 vouchers: list[Voucher] = []
+deck = Deck()
+hand: Hand = Hand([])
+
+for suit in Suits:
+    for face in Faces:
+        new_card = Card(card_values[face.name], face.name, suit.name, '', '', '')
+        deck.add_card(new_card)
+
+
+for i in range(0,7):
+    hand.cards.append(deck.cards.pop(0))
+
+for i in hand.cards:
+    print(i)
+
+input('something')
+
+PlayedHand()
